46d229bd75b2140933ac02483f279574a3398fde
abplayzGH <95254250+abplayzGH@users.noreply.github.com>
2023-02-22 11:15:14 -0600

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiNmY4NjE0NjgxMjMzMzM3YzE5N2U5NWI2ZGZhN2QxN2YiLCJ0IjoiYzJiMzgzMzctMjQzMy00NjQxLWE4MTQtZjliNDAzNzIxZjEyIiwicyI6IlkyVXhNakV3WmpBdFlXRTBOUzAwTlRZeExUaGhaR0l0WkRGaE1qQXlZelZrTkRFdyJ9
